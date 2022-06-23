3e7e7caf5a25421875ef94cee961c4ab1d0ed920
Dristi1210sahu <89518155+Dristi1210sahu@users.noreply.github.com>
2022-06-23 15:55:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://storage.googleapis.com/tm-model/3yZau5WLU/model.json', modelReady );
+
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+    
+}
